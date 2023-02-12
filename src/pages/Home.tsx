3e7ecc0f18e20d72ac41168a06b41ce71666ffce
mohammad maso <mohammadmasoudie@gmail.com>
2023-02-12 08:50:36 +0330

    new build 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Surveys from '../components/Surveys';
 const Home = () => (
   <div className="sjs-client-app__content--surveys-list">
     <h1>نظرسنجی‌ها</h1>
-    <Surveys />
+    {/* <Surveys /> */}
   </div>
 );
 
